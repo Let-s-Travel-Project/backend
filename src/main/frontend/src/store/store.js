d034d5ec8b175e7bbfb7bde500672a4f9085221c
Choi, Suyeong <74814586+suy0594@users.noreply.github.com>
2024-11-20 18:13:56 +0900

    frontend 파일 삭제

--- src/main/frontend/src/store/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import scheduleReducer from "./schedule/scheduleSlice";
-import navigationReducer from "./navigation/navigationSlice";
-export const store = configureStore({
-  reducer: { schedule: scheduleReducer, navigation: navigationReducer },
-});
--- src/main/frontend/src/util/getDateDif.js
+++
@@ -1,7 +0,0 @@
-export const getDaysDifference = (startDate, endDate) => {
-  const oneDay = 24 * 60 * 60 * 1000; // 하루의 밀리초
-  const start = new Date(startDate);
-  const end = new Date(endDate);
-  const diffInMilliseconds = end - start;
-  return Math.round(diffInMilliseconds / oneDay);
-};
