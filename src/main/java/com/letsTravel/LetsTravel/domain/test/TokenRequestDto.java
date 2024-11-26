2670cf07cbe02e6d841c31b9ba9f1dd830288352
Choi, Suyeong <tndud25@gmail.com>
2024-11-26 15:43:09 +0900

    feat: 토큰 통신을 위한 DTO 작성

---
+++ src/main/java/com/letsTravel/LetsTravel/domain/test/TokenRequestDto.java
@@ -0,0 +1,5 @@
+package com.letsTravel.LetsTravel.domain.test;
+
+public class TokenRequestDto {
+    public String token;
+}
---
+++ src/main/java/com/letsTravel/LetsTravel/domain/test/TokenResponse.java
@@ -0,0 +1,6 @@
+package com.letsTravel.LetsTravel.domain.test;
+
+public class TokenResponse {
+    public String accessToken;
+    public int expiresInSeconds;
+}
