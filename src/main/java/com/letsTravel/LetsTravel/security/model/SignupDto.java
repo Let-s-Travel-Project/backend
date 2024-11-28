81c420e017f52dfb36b49fc768c46463ec3a8a48
Choi, Suyeong <tndud25@gmail.com>
2024-11-28 18:17:09 +0900

    feat: SignupDto 필드 추가

--- src/main/java/com/letsTravel/LetsTravel/security/model/SignupDto.java
+++ src/main/java/com/letsTravel/LetsTravel/security/model/SignupDto.java
@@ -2,10 +2,18 @@ package com.letsTravel.LetsTravel.security.model;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class SignupDto {
 
     private String username;
     private String password;
+    private String firstName;
+    private String lastName;
+    private String nickname;
+    private Date birth = null;
+    private Byte sex = null;
+    private boolean status = true;
 
 }
