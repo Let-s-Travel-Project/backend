228af7e363a1e6d6f46ca1eb1c384d2e6731a0ab
Choi, Suyeong <tndud25@gmail.com>
2024-11-30 18:00:21 +0900

    delete: 백업 폴더 삭제

--- src/main/java/com/letsTravel/LetsTravel/backup/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.letsTravel.LetsTravel.backup.repository;
-
-import java.util.List;
-
-import com.letsTravel.LetsTravel.backup.domain.member.MemberBasicInfoReadDTO;
-
-public interface MemberRepository {
-	List<MemberBasicInfoReadDTO> findPlanShareMemberByPlanSeq(int planSeq);
-}
