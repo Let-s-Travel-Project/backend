257fe1e9af96d48a802dddd40446a6cf429c8e7e
KoharuDaisukii <bong1926@naver.com>
2024-12-02 18:50:49 +0900

    PageParamDto로 form data를 받도록 변경

--- src/main/java/com/letsTravel/LetsTravel/controller/map/CountryController.java
+++ src/main/java/com/letsTravel/LetsTravel/controller/map/CountryController.java
@@ -2,13 +2,10 @@ package com.letsTravel.LetsTravel.controller.map;
 
 import com.letsTravel.LetsTravel.domain.map.dto.MapDto;
 import com.letsTravel.LetsTravel.domain.map.entity.Country;
+import com.letsTravel.LetsTravel.domain.util.PageParamDto;
 import com.letsTravel.LetsTravel.service.map.CountryService;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,15 +18,10 @@ public class CountryController {
 
 	private final CountryService countryService;
 
-	@GetMapping("/list")
-	public ResponseEntity<List<Country>> getAllCountries(
-			@RequestParam(value = "page") int page,
-			@RequestParam(value = "size") int size,
-			@RequestParam(value = "orderby") String orderBy,
-			@RequestParam(value = "sort") String sort
-			) {
-		Pageable pageable = sort.equals("DESC") ? PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, orderBy)): PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, orderBy));
-		return ResponseEntity.ok(countryService.getAllCountries(pageable).getContent());
+	@GetMapping("/")
+	public ResponseEntity<List<Country>> getAllCountries(@ModelAttribute("pageParamDto") PageParamDto pageParamDto) {
+		System.out.println(pageParamDto);
+		return ResponseEntity.ok(countryService.getAllCountries(pageParamDto));
 	}
 
 	@GetMapping
